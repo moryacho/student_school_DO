12c9452acd6714d900d5a1e837953e62c7fe69da
yanaz <jdbsjrv@gmail.com>
2023-10-14 14:03:49 +0300

    task

--- student_school_DO/practice_14_10/Program.cs
+++ student_school_DO/practice_14_10/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+static string FooBar(int num)
+{
+    string res = "";
+    if (num % 3 == 0)
+    {
+        res += "foo";
+    }
+    if (num % 5 == 0)
+    {
+        res += "bar";
+    }
+
+    return res;
+}
+
+
+Console.Write("Input final number: ");
+int end = int.Parse(Console.ReadLine());
+for (int i = 1; i <= end; i++)
+{
+    Console.WriteLine(i + " " + FooBar(i));   
+}
+Console.ReadLine();
